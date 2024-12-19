f5202fd496512a23b0c7a1dabf057ae15b810769
Hannes Rauscher <hannes.rauscher@gmx.at>
2024-12-19 12:21:26 +0100

    completed tasks

--- ConsolePong/PointSystem.cs
+++ ConsolePong/PointSystem.cs
@@ -14,7 +14,9 @@ namespace ConsolePong
          * Gib den Wert der Felder in der DrawScores() Methode an der entsprechenden stelle aus
         */
 
-        // TODO A_1
+        private int playerLeftScore = 0; // TODO A_1
+        private int playerRightScore = 0; // TODO A_1
+        private int playerTopScore = 0; // TODO A_1
 
         private Ball ball;
 
@@ -29,8 +31,7 @@ namespace ConsolePong
 
         private int newBallDelay = 1500;
 
-        //Konstruktor
-        public PointSystem(Ball ball, Paddle paddleLeft, ConsoleColor paddleLeftColor, Paddle paddleRight, ConsoleColor paddleRightColor, 
+        public PointSystem(Ball ball, Paddle paddleLeft, ConsoleColor paddleLeftColor, Paddle paddleRight, ConsoleColor paddleRightColor,
             Vector2D fieldSize, int paddleOffset, ConsoleColor ballColor)
         {
             /* T4-Klasse 05 - Aufgabe_2
@@ -39,6 +40,14 @@ namespace ConsolePong
             */
 
             // TODO A_2
+            this.ball = ball;
+            this.paddleLeft = paddleLeft;
+            this.paddleLeftColor = paddleLeftColor;
+            this.paddleRight = paddleRight;
+            this.paddleRightColor = paddleRightColor;
+            this.fieldSize = fieldSize;
+            this.paddleOffset = paddleOffset;
+            this.ballColor = ballColor;
         }
 
         // Konsolenausgabe
@@ -47,10 +56,10 @@ namespace ConsolePong
             ConsoleColor foregroundColor = Console.ForegroundColor;
             Console.ForegroundColor = paddleLeftColor;
             Console.SetCursorPosition(paddleOffset - 1 + (fieldSize.X / 2 - paddleOffset) / 2, 1);
-            Console.Write("playerLeftScore");
+            Console.Write(playerLeftScore);
             Console.ForegroundColor = paddleRightColor;
             Console.SetCursorPosition(fieldSize.X / 2 + (fieldSize.X / 2 - paddleOffset) / 3, 1);
-            Console.Write("playerRightScore");
+            Console.Write(playerRightScore);
             Console.ForegroundColor = foregroundColor;
         }
 
@@ -85,8 +94,8 @@ namespace ConsolePong
              * - PointReset()
              * - fieldSize.X
             */
-            if (ballPosition.X == null) { /* TODO A_3 */ return "right player score test string (do not delete)"; }
-            if (ballPosition.X == null) { /* TODO A_3 */ return "left player score test string (do not delete)"; }
+            if (ballPosition.X == 0) { playerRightScore++; PointReset(); return "right player score test string (do not delete)"; } // TODO A_3
+            if (ballPosition.X == fieldSize.X - 1) { playerLeftScore++; PointReset(); return "left player score test string (do not delete)"; } // TODO A_3
             return "no return test string (do not delete)";
         }
     }
