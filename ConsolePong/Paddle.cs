8f5062e12851d2ab0292846f9984722ebd8ac5e1
Hannes Rauscher <hannes.rauscher@gmx.at>
2025-01-16 11:47:51 +0100

    finished all exercises in paddle class

--- ConsolePong/Paddle.cs
+++ ConsolePong/Paddle.cs
@@ -19,29 +19,34 @@ namespace ConsolePong
 
 
         /* T2-Klasse 03 - Aufgabe_1
-         * 
          * Implementiere die zwei gegebenen Properties (Eigenschaften), welche je folgende Werte zurückliefern sollen:
          * - positionNew
          * - size
         */
 
-        public Vector2D Position => throw new NotImplementedException("Die Property 'Position' wurde noch nicht in Paddle implementiert"); // TODO A_1
-        public int Size => throw new NotImplementedException("Die Property 'Size' wurde noch nicht in Paddle implementiert"); // TODO A_1
+        public int Size { get { return size; } } // TODO A_1
+        public Vector2D Position { get { return positionNew; } } // TODO A_1
 
         // Konstruktor:
-        public Paddle (char character, int size, ConsoleColor color, Vector2D position, int speed, Vector2D fieldSize)
+        public Paddle(char character, int size, ConsoleColor color, Vector2D position, int speed, Vector2D fieldSize)
         {
             /* T2-Klasse 03 - Aufgabe_2
-             * 
              * Implementiere den Konstruktor, sodass die übergebenen Werte in die Felder der Klasse gespeichert werden
-             * 
              * Folgendes ist zu beachten:
-             * - size darf minimal den Wert 3 erhalten (wenn size < 3, soll sie auf 3 gesetzt werden)
-             * - alle position-Felder (start/old/new) übernehmen den gleichen Initialwert (Vector2D position)
+             * - size darf minimal den Wert 3 erhalten
+             * - alle position-Felder (old/new/start) übernehmen den gleichen Initialwert
             */
 
             // TODO A_2
-
+            this.character = character;
+            if (size < 3) size = 3;
+            this.size = size;
+            this.color = color;
+            positionStart = position;
+            positionNew = positionStart;
+            positionOld = positionStart;
+            this.speed = speed;
+            this.fieldSize = fieldSize;
         }
 
         // Aktualisierung der Position
@@ -51,27 +56,26 @@ namespace ConsolePong
             {
                 /* T2-Klasse 03 - Aufgabe_3
                  * 
-                 * Für den Fall "up" soll sich der Schläger nach oben bewegen.
-                 * Für den Fall "down" soll sich der Schläger nach unten bewegen.
-                 * Dazu wird die alte Position des Schlägers genommen und mit der gespeicherten Geschwindikeit (speed) angepasst.
-                 * Addiere oder Subtrahiere (+ / -) speed von positionOld.Y, sodass das gewünschte Verhalten auftritt!
+                 * Berechne die Bewegung des Schlägers je nach übergebenem string nach oben oder unten
+                 * Der Schläger soll sich mit der gespeicherten Geschwindikeit (speed) bewegen
                  * 
-                 * TIPP: Achte auf den Aufbau des 
+                 * Zu verwendende Methoden und Eigenschaften:
+                 * - positionNew
+                 * - new Vector2D
+                 * - positionOld.X
+                 * - positionOld.Y
+                 * - speed
                 */
 
                 case "up":
-
-                    // TODO A_3
-                    positionNew.Y = positionOld.Y; // (+ / -) speed
-
-                    if (positionNew.Y < 0) positionNew.Y = 0;  // NOTE: überprüft ob der Schläger den oberen Rand erreicht, wenn ja -> Bewegung/Position wird zurückgesetzt
+                    positionNew = new Vector2D(positionOld.X, positionOld.Y - speed); // TODO A_3
+                    if (positionNew.Y < 0) positionNew.Y = 0;
+                    // NOTE: überprüft ob der Schläger den oberen Rand erreicht, wenn ja -> Bewegung/Position wird zurückgesetzt
                     break;
                 case "down":
-
-                    // TODO A_3
-                    positionNew.Y = positionOld.Y; // (+ / -) speed
-
-                    if (positionNew.Y > fieldSize.Y - size) positionNew.Y = fieldSize.Y - size; // NOTE: überprüft ob der Schläger den unteren Rand erreicht, wenn ja -> Bewegung/Position wird zurückgesetzt
+                    positionNew = new Vector2D(positionOld.X, positionOld.Y + speed); // TODO A_3
+                    if (positionNew.Y > fieldSize.Y - size) positionNew.Y = fieldSize.Y - size;
+                    // NOTE: überprüft ob der Schläger den unteren Rand erreicht, wenn ja -> Bewegung/Position wird zurückgesetzt
                     break;
             }
         }
